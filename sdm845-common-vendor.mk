15e56a393cd3012b2f180def39b5572af86c8fde
Canary233 <2096276316@qq.com>
2022-08-04 17:13:31 +0800

    sdm845-common:Drop libplatformconfig

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -825,7 +825,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm845-common/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
-    libplatformconfig \
     CneApp \
     IWlanService \
     SoterService \
